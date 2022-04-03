33cdb829c0566115ee6a369d2eba2d8515f7bee2
EvanOrunto <evanyinkaorunto@yahoo.com>
2022-04-03 07:35:13 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab1ass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello Evan,i am in backend development of Wella Schools");
+           Console.WriteLine("I am learning backend development because i want to get acquainted with several programming languages, which can give my career a great boost");
+        }
+    }
+}
